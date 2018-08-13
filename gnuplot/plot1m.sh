ed521a23f8c5a06af1d57a4b24133f3d5a35dc1c
Thomas Eichhorn <thomas.eichhorn@cern.ch>
2018-08-13 14:45:06 +0200

    Beautified

--- gnuplot/plot1m.sh
+++ gnuplot/plot1m.sh
@@ -10,28 +10,29 @@ pastyear=$(date --date='1 month ago' +%Y)
 pastmonth=$(date --date='1 month ago' +%m)
 pastday=$(date --date='1 month ago' +%d)
 pasthour=$(date --date='1 month ago' +%H)
-echo $yeartoday $monthtoday $daytoday $hourtoday
-echo $pastyear $pastmonth $pastday $pasthour
+
 gnuplot <<-EOFMarker
 set term x11 persist
 set xdata time
 set timefmt "%Y-%m-%d %H:%M:%S"
 set format x "%d.%m %H:%M"
 set xrange ["$pastyear-$pastmonth-$pastday $pasthour:00:00":"$yeartoday-$monthtoday-$daytoday $hourtoday:00:00"]
-set yrange [18:24]
-set y2range [0.1:50]
-set log y2
+set yrange [16:27]
+set y2range [0.1:100]
+set key font ",20"
+#set log y2
 set grid x y
-set xlabel "Time"
-set ylabel "Deg C"
-set y2label "% RH"
+set xlabel "Time" font ",20"
+set ylabel "Deg C" font ",20"
+set y2label "% RH" font ",20"
 set ytics nomirror
-set y2tics
-set mytics 0.5
+set xtics font ",20"
+set ytics font ",20"
+set y2tics font ",20"
+set mytics 5
 set tics out
-set title "Status of $machine - past month"
+set title "Status of $machine - past month" font ",20"
 #set autoscale y
 #set autoscale y2
-plot "/opt/measurements/readings_log.txt" using 1:3 title "Temperature" w p, "/opt/measurements/readings_log.txt" using 1:4 title "% Relative Humidity" w p axes x1y2
+plot "/opt/measurements/readings_log.txt" using 1:3 title "Temperature" w l lt rgb "red"  lw 3, "/opt/measurements/readings_log.txt" using 1:4 title "% Relative Humidity" w l lt rgb "blue"  lw 3 axes x1y2
 EOFMarker
-echo "test"
